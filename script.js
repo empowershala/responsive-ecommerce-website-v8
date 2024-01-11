ecbba4c9ff8408784b07604a82a2a01c74638f6b
Empowershala <131361027+empowershala@users.noreply.github.com>
2024-01-11 13:43:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+
+// Swiper Initialization
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
+// Hamburger menu
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('ri-close-line');
+    navbar.classList.toggle('active');
+}
+
+// Scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+
+            // active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    // sticky header
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle button and navbar when click navbar links (scroll) 
+    menuIcon.classList.remove('ri-close-line');
+    navbar.classList.remove('active');
+}
+
+// ScrollReveal animations
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal(`.home-left`, { origin: 'left' })
+sr.reveal(`.home-right`, { origin: 'right', delay: 1200})
+sr.reveal(`.feature-box`, { origin: 'left', delay: 2000, interval: 600 })
+sr.reveal(`.explore-box`, { origin: 'left', interval: 1000 })
+sr.reveal(`.signup-content-left`, { origin: 'right' })
+sr.reveal(`.signup-content-right`, { origin: 'left', delay: 1200 })
+sr.reveal(`.swiper`, { origin: 'left' })
+sr.reveal(`.footer-box`, { origin: 'left', interval: 1000 })
